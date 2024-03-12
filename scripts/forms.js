e77241d69f95f0a8fd3a603a4323f43706ee053a
Colby Wemer <cgwemer@gmail.com>
2024-03-12 02:41:35 -0500

    fixed form

--- scripts/forms.js
+++ scripts/forms.js
@@ -27,8 +27,10 @@ function checkDate() {
   var startDate = new Date(startDateInput.value);
   var endDateInput = document.getElementById("date2");
   var endDate = new Date(endDateInput.value);
-  endDateInput.setAttribute("min", startDate.toISOString().slice(0, 16));
-
+  endDateInput.setAttribute(
+    "min",
+    new Date(startDate.getTime() - startDate.getTimezoneOffset() * 60000).toISOString().slice(0, 16)
+  );
   // Check if end time is not set or if it's set to a date before start time
   if (!endDateInput.value || endDate < startDate) {
     // Set end time to start time
